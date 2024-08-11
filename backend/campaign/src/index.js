e9c064a49b965ebf02ceb24f5dfd7ffe17833567
Thuận Phát <phat06112003@gmail.com>
2024-08-11 09:02:02 +0700

    Initialize game and campaign

---
+++ backend/campaign/src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const port = process.env.PORT; 
+const dbURI = 'mongodb://127.0.0.1:27017/QuizUs'; //process.env.
+
+const app = express();
+app.use(express.json());
+
+app.use('/', (req, res) => {
+  res.send("OK");
+});
+
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to database');
+}).catch(err => {
+  console.error('Failed to connect to database:', err);
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
