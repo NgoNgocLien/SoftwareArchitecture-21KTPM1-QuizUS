598f114ec53b3fbd0ff4e4fd834d92744157107e
NgoNgocLien <nnlien21@clc.fitus.edu.vn>
2024-08-12 13:46:00 +0700

    top bar homepage

--- quizus/app/(tabs)/favorite.tsx
+++ quizus/app/(tabs)/favorite.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Keyboard, TouchableWithoutFeedback, View } from 'react-native';
+import { StyleSheet, Keyboard, TouchableWithoutFeedback, View, Text } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 export default function Favorite() {
@@ -9,20 +9,84 @@ export default function Favorite() {
             locations={[0, 0.49, 0.79]} // Start the gradient at 49% and end at 79%
             style={styles.background}
         >
-            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-                <View>
-                    
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>                
+                <View style={styles.legSummary}>
+                    <View style={styles.legSummaryTop}>
+                        <View>
+                            <Text style={styles.legTime}>10:30</Text>
+                            <Text style={styles.arrivalDepartureAirport}>SFO</Text>
+                        </View>
+                        <View>
+                            <Text>10:30</Text>
+                            <Text>SFO</Text>
+                        </View>
+                        <View>
+                            <Text style={styles.legTime}>10:30</Text>
+                            <Text style={styles.arrivalDepartureAirport}>EWR</Text>
+                        </View>
+                    </View>
+                    <View style={styles.legSummaryBottom}></View>
+                    <View style={[styles.leftCut, styles.ticket]}></View>
+                    <View style={[styles.rightCut, styles.ticket]}></View>
                 </View>
             </TouchableWithoutFeedback>
         </LinearGradient>
-    )
-}
+  );
+};
 
 const styles = StyleSheet.create({
+
     background: {
-      flex: 1,
-    },
-    container: {
-        paddingHorizontal: 20,
-    },
+        flex: 1,
+      },
+      container: {
+          paddingHorizontal: 20,
+      },
+
+  legSummary: {
+    marginTop: 10
+  },
+  legSummaryTop: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    height: 80,
+    paddingTop: 24,
+    backgroundColor: "grey",
+    borderTopRightRadius: 16,
+    borderTopLeftRadius: 16,
+    paddingHorizontal: 16
+  },
+  legSummaryBottom: {
+    height: 80,
+    backgroundColor: "grey",
+    borderBottomRightRadius: 16,
+    borderBottomLeftRadius: 16
+  },
+  ticket: {
+    position: "absolute",
+    bottom: 56,
+    // transform: "translateY(50%)",
+    borderTopColor: "transparent",
+    borderTopWidth: 12,
+    borderBottomColor: "transparent",
+    borderBottomWidth: 12
+  },
+  leftCut: {
+    borderLeftColor: "#F3F8FE",
+    borderLeftWidth: 12
+  },
+  rightCut: {
+    right: 0,
+    borderRightColor: "#F3F8FE",
+    borderRightWidth: 12
+  },
+  legTime: {
+    fontWeight: 600,
+    fontSize: 18,
+    lineHeight: 20
+  },
+  arrivalDepartureAirport: {
+    fontSize: 12,
+    marginTop: 8
+  }
 });
