82b9983a77c777b80ec19304b283a7b85ad049fe
Thuận Phát <phat06112003@gmail.com>
2024-09-04 22:00:29 +0700

    brand signup

--- backend/user/src/controllers/brandController.js
+++ backend/user/src/controllers/brandController.js
@@ -7,4 +7,25 @@ const { Op, literal, fn, col } = require("sequelize");
 
 const { successCode, failCode, errorCode } = require('../config/response');
 
-module.exports = {}
+const signup = async (req, res) => {
+  try {
+    const { name, field, address, lat, long, username, pwd, phone, email } = req.body;
+
+    const existingBrand = await model.brand.findOne({ where: { [Op.or]: [{ username }, { email }] } });
+
+    if (existingBrand) {
+      return failCode(res, null, 'Username hoặc email đã tồn tại.');
+    }
+
+    const newBrand = await model.brand.create({
+      name, field, address, lat, long, is_active: true, username, pwd, phone, email
+    });
+
+    return successCode(res, newBrand, 'Đăng ký brand thành công');
+  } catch (error) {
+    console.error('Error:', error);
+    return errorCode(res);
+  }
+}
+
+module.exports = { signup }
--- backend/user/src/routes/brandRoute.js
+++ backend/user/src/routes/brandRoute.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const brandRoute = express.Router();
 
+const { signup } = require('../controllers/brandController');
+
+brandRoute.post('/signup', signup);
+
 module.exports = brandRoute
