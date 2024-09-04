a7cda7a1f1980180e79f95b046cda946bd4910d8
pmlinh21 <pmlinh21@clc.fitus.edu.vn>
2024-09-04 11:24:38 +0700

    update player backend

--- backend/user/src/routes/playerRoute.js
+++ backend/user/src/routes/playerRoute.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const playerRoute = express.Router();
-const {signup, otp, login} = require('../controllers/playerController');
+const {signup, otp, login,
+    getAll, update
+} = require('../controllers/playerController');
 
 playerRoute.post('/login', login);
 
 playerRoute.post('/signup', signup);
 
 playerRoute.post('/otp', otp);
+
+playerRoute.get('/', getAll);
+
+playerRoute.put('/', update);
 
 module.exports = playerRoute
