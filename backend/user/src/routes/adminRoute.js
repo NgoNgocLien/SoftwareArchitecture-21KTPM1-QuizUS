56ec9cfe3629c16ae568eccda03a031f84530837
Thuận Phát <phat06112003@gmail.com>
2024-09-05 21:52:40 +0700

    admin api

--- backend/user/src/routes/adminRoute.js
+++ backend/user/src/routes/adminRoute.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const adminRoute = express.Router();
 
+const { createAdmin, updateAdmin, deleteAdmin } = require('../controllers/adminController');
+
+adminRoute.post('/', createAdmin);
+
+adminRoute.put('/', updateAdmin);
+
+adminRoute.delete('/', deleteAdmin);
+
 module.exports = adminRoute
