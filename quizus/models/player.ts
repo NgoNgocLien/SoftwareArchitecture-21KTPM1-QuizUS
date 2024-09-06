ec061af9b93ae9f7066c616cdeea3e84f017fb6b
NgoNgocLien <nnlien21@clc.fitus.edu.vn>
2024-09-06 13:49:07 +0700

    update port from 808x to 800x

---
+++ quizus/models/player.ts
@@ -0,0 +1,12 @@
+interface IPlayer {
+    id_player: string;
+    username: string;
+    avatar: string;
+    dob: string;
+    email: string;
+    phone: string;
+    gender: string;
+    facebook: string;
+    is_active: boolean;
+    score: number;
+}
