8ea6f37e4ba0116b918e4e05fbff2e3372f42c70
FelinTran <felintran.work@gmail.com>
2024-09-06 22:50:17 +0700

    done user

--- admin/src/pages/ManageUser.jsx
+++ admin/src/pages/ManageUser.jsx
@@ -3,37 +3,68 @@ import "../styles/common.css";
 import "../styles/manage.css";
 
 export default function ManageUser() {
-    <div className="manage-user">
-        <h3>Quản lý người chơi</h3>
-        <table className="user-table">
-            <thead>
-                <tr>
-                    <th><input type="checkbox" /></th>
-                    <th>Họ tên</th>
-                    <th>Email</th>
-                    <th>Điện thoại</th>
-                    <th>Giới tính</th>
-                    <th>Ngày sinh</th>
-                    <th>Facebook</th>
-                </tr>
-            </thead>
-            <tbody>
-            {/* Row */}
-                <tr>
-                    <td><input type="checkbox" /></td>
-                    <td>
-                        <div className="user-info">
-                            <img src="/icons/user-avatar.svg" alt="user-avatar" className="user-avatar" />
-                            <span>Nguyễn Văn A</span>
-                        </div>
-                    </td>
-                    <td>email@example.com</td>
-                    <td>0901234567</td>
-                    <td>Nam</td>
-                    <td>01/01/1990</td>
-                    <td><a href="https://facebook.com">Facebook Link</a></td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+    // const handleEdit = (id) => {
+    //     console.log('Chỉnh sửa người chơi có ID: ', id);
+    // };
+
+    // const handleDelete = (id) => {
+    //     console.log('Xóa người chơi có ID: ', id);
+    // };
+
+    return (
+        <div>
+            {/* Search bar */}
+
+            <div className='manage-user'>
+                <table className="user-table">
+                    <thead>
+                        <tr>
+                            <th><input type="checkbox" />ID</th>
+                            <th>Họ tên</th>
+                            <th>Email</th>
+                            <th>Điện thoại</th>
+                            <th>Giới tính</th>
+                            <th>Ngày sinh</th>
+                            <th>Hành động</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {/* Row */}
+                        <tr>
+                            <td><input type="checkbox" />#123456</td>
+
+                            <td>
+                                <div className="user-info">
+                                    <img src="/images/user-avatar.svg" alt="user-avatar" className="user-avatar" />
+                                    <span>Nguyen Van A</span>
+                                    {/* <span>{user.name}</span> */}
+                                </div>
+                            </td>
+                            
+                            <td>email@example.com</td>
+                            <td>0901234567</td>
+                            <td>Nam</td>
+                            <td>01/01/1990</td>
+                            {/* <td>{user.email}</td>
+                            <td>{user.phone}</td>
+                            <td>{user.gender}</td>
+                            <td>{user.birthdate}</td>*/}
+
+                            <td className='action-buttons'>
+                                <button className="edit-btn">
+                                    <img src="/icons/edit.svg" alt="edit-btn" />
+                                    Sửa
+                                </button>
+                                <button className="delete-btn">
+                                    <img src="/icons/delete.svg" alt="delete-btn" />
+                                    Xóa
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            
+        </div>
+    )
 }
