00cb091de48f8292e8e5ecf9d44db94a0f2f6f92
hongphuc14 <nthphuc21@clc.fitus.edu.vn>
2024-09-06 22:21:50 +0700

    api exchange coin in user service

--- backend/campaign/src/routes/voucherRoutes.js
+++ backend/campaign/src/routes/voucherRoutes.js
@@ -152,4 +152,40 @@ router.get('/exchange/:id_player', async (req, res) => {
     }
 });
 
+// đổi voucher
+router.post('/exchange/coin', async (req, res) => {
+  try {
+      const { id_player, id_campaign, score_exchange } = req.body;
+
+      if (!id_player || !id_campaign || !score_exchange) {
+          return res.status(400).json({ message: 'id_player, id_campaign, and score_exchange are required' });
+      }
+
+      try {
+          const response = await axios.put('http://gateway_proxy:8000/user/api/player/coin', {
+              id_player,
+              score: score_exchange
+          });
+
+          if (response.status === 200) {
+              const playerVoucher = new PlayerVoucher({
+                  id_player,
+                  id_campaign,
+                  is_used: false
+              });
+
+              const savedPlayerVoucher = await playerVoucher.save();
+              return res.status(201).json(savedPlayerVoucher);
+          } else {
+              return res.status(500).json({ message: 'Failed to deduct coins from player.' });
+          }
+
+      } catch (error) {
+          return res.status(500).json({ message: 'Failed to communicate with the coin deduction service.', error: error.message });
+      }
+  } catch (error) {
+      res.status(500).json({ message: 'Server error', error: error.message });
+  }
+});
+
 module.exports = router;
