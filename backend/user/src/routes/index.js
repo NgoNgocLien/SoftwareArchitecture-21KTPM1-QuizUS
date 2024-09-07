e57fb4fcc68edeeceb0a1d1aaa802ccf4a40b5cd
hongphuc14 <nthphuc21@clc.fitus.edu.vn>
2024-09-07 15:07:42 +0700

    update api user

--- backend/user/src/routes/index.js
+++ backend/user/src/routes/index.js
@@ -42,4 +42,41 @@ rootRoute.post("/login", async (req, res) => {
     }
 })
 
+rootRoute.post("/loginWeb", async (req, res) => {
+    try{
+        const {email, pwd} = req.body;
+
+        const admin = await model.admin.findOne({ where: { email: email } });
+
+        if (!admin) {
+            const brand = await model.brand.findOne({ where: { email: email } });
+            if(!brand){
+                return failCode(res, null, "Email không tồn tại");
+            }
+            const isMatch = await bcrypt.compare(pwd, brand.pwd);
+
+            if (isMatch) {
+                const brandData = { ...brand.toJSON() };
+                delete brandData.pwd;
+                successCode(res, brandData, "Đăng nhập thành công");
+            } else {
+                failCode(res, null, "Mật khẩu brand không chính xác");
+            }
+        }
+
+        const isMatch = await bcrypt.compare(pwd, admin.pwd);
+
+        if (isMatch) {
+            const adminData = { ...admin.toJSON() };
+            delete adminData.pwd;
+            successCode(res, adminData, "Đăng nhập thành công");
+        } else {
+            failCode(res, null, "Mật khẩu admin không chính xác");
+        }
+    }catch(err){
+        console.log(err)
+        errorCode(res)
+    }
+})
+
 module.exports = rootRoute;
