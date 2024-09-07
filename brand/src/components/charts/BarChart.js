34d5e32b4f4127113002a5944ff85ee4b9995d4d
FelinTran <felintran.work@gmail.com>
2024-09-07 17:43:55 +0700

    edit sidebar & add brand dashboard

---
+++ brand/src/components/charts/BarChart.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+
+// Register the chart components
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+function BarChart() {
+    const barData = {
+        labels: ['Tháng 1', 'Tháng 2', 'Tháng 3', 'Tháng 4', 'Tháng 5', 'Tháng 6', 'Tháng 7', 'Tháng 8'],
+        datasets: [
+          {
+            label: 'Nhà hàng',
+            backgroundColor: '#34C759',
+            borderColor: '#34C759',
+            borderWidth: 1,
+            data: [2000000, 4000000, 6000000, 8000000, 10000000, 6000000, 4000000, 2000000],
+          },
+          {
+            label: 'Cafe và bánh',
+            backgroundColor: '#FF9500',
+            borderColor: '#FF9500',
+            borderWidth: 1,
+            data: [4000000, 6000000, 8000000, 6000000, 4000000, 2000000, 10000000, 8000000],
+          },
+          {
+            label: 'Mua sắm',
+            backgroundColor: '#FF3B30',
+            borderColor: '#FF3B30',
+            borderWidth: 1,
+            data: [6000000, 8000000, 10000000, 8000000, 6000000, 4000000, 2000000, 10000000],
+          },
+          {
+            label: 'Giải trí',
+            backgroundColor: '#007AFF',
+            borderColor: '#007AFF',
+            borderWidth: 1,
+            data: [10000000, 8000000, 6000000, 4000000, 2000000, 10000000, 8000000, 6000000],
+          },
+        ],
+      };
+      
+      const barOptions = {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          x: {
+            title: {
+              display: true,
+              text: 'Tháng',
+            },
+          },
+          y: {
+            title: {
+              display: true,
+              text: 'Ngân sách (VNĐ)',
+            },
+            ticks: {
+              callback: (value) => `${(value / 1000000)} triệu`,
+            },
+            min: 0,
+            max: 10000000,
+            stepSize: 2000000,
+          },
+        },
+        plugins: {
+          legend: {
+            display: true,
+            position: 'bottom',
+          },
+          tooltip: {
+            callbacks: {
+              label: (tooltipItem) => `${tooltipItem.dataset.label}: ${tooltipItem.raw.toLocaleString()} VNĐ`,
+            },
+          },
+        },
+        
+
+    }   
+
+    return (
+        <div style={{ width: '100%', height: '900px' }}>
+          <Bar data={barData} options={barOptions} />
+        </div>
+    );
+}
+
+export default BarChart;
+
+  
---
+++ brand/src/pages/Dashboard.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import Chart from "chart.js/auto";
+import LineChart from '../components/charts/LineChart';
+import VoucherChart from '../components/charts/VoucherChart';
+import PlayerChart from '../components/charts/PlayerChart';
+import BarChart from '../components/charts/BarChart';
+import "../styles/common.css";
+import "../styles/dashboard.css";
+
+export default function Dashboard() {
+    return (
+        <div>
+            {/* Số lượng */}
+            <div className='total-ctn'>
+                <div className='total d-flex flex-column'>
+                    <p>Tổng nhãn hàng</p>
+                    <h5>2420</h5>
+                </div>
+                <div className='total d-flex flex-column'>
+                    <p>Tổng sự kiện</p>
+                    <h5>2420</h5>
+                </div>
+                <div className='total d-flex flex-column'>
+                    <p>Tổng người chơi</p>
+                    <h5>2420</h5>
+                </div>
+                <div className='total d-flex flex-column'>
+                    <p>Tổng mã giảm giá</p>
+                    <h5>2420</h5>
+                </div>
+            </div>
+
+            {/* Thống kê */}
+            <div className='statistics-ctn gap-2'>
+                {/* Thống kê người chơi */}
+                <div className='user-chart'>
+                    <p>Thống kê</p>
+                    <h6>Người chơi</h6>
+                    <LineChart />
+                </div>
+                
+                <div className='piechart d-flex flex-column'>
+                    {/* Thống kê tình trạng vouchers */}
+                    <div className='voucher-chart'>
+                        <p>Thống kê</p>
+                        <h6>Tình trạng voucher</h6>
+                        <VoucherChart />
+                    </div>
+                    {/* Thống kê người chơi theo loại trò chơi */}
+                    <div className='player-chart'>
+                        <p>Thống kê</p>
+                        <h6>Người chơi theo loại trò chơi</h6>
+                        <PlayerChart />
+                    </div>
+                </div>
+                
+                
+            </div>
+            
+            {/* Thống kê ngân sách */}
+            <div className='budget-chart'>
+                <p>Thống kê</p>
+                <h6>Ngân sách đã sử dụng theo lĩnh vực</h6>
+                <BarChart />
+            </div>
+        </div>
+    );
+};
