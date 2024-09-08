2a0b01968bd377f8c7dc7607730548e84296bbc4
hongphuc14 <nthphuc21@clc.fitus.edu.vn>
2024-09-08 17:22:15 +0700

    brand info on topbar

--- brand/src/components/topbar/Topbar.jsx
+++ brand/src/components/topbar/Topbar.jsx
@@ -17,12 +17,15 @@ export default function Topbar() {
         }
     };
 
+    const storedBrand = localStorage.getItem('brand');
+    const brand = storedBrand ? JSON.parse(storedBrand) : null;
+
     return (
         <div className="topbar d-flex flex-row justify-content-between">
             <h6>{getPageName(location.pathname)}</h6>
             <div className="d-flex flex-row user-profile align-items-center">
-                <img src="/icons/avatar.svg" alt="user-avatar" />
-                <p>Brand</p>
+                <img src={brand.logo ? brand.logo : "/icons/avatar.svg"} alt="user-avatar" />
+                <p>{brand.name}</p>
             </div>
         </div>
     )
