734c6714afacdb7012a28a1bbe9f028f78abcb80
hongphuc14 <nthphuc21@clc.fitus.edu.vn>
2024-09-08 15:48:55 +0700

    add logout

--- admin/src/components/topbar/Topbar.jsx
+++ admin/src/components/topbar/Topbar.jsx
@@ -1,14 +1,17 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import "./Topbar.css";
 
 export default function Topbar() {
     const location = useLocation();
+    const navigate = useNavigate();
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
     const getPageName = (pathname) => {
         switch (pathname) {
             case '/dashboard':
                 return 'Tổng Quan';
-            case '/user':
+            case '/player':
                 return 'Người Chơi';
             case '/brand':
                 return 'Nhãn Hàng';
@@ -19,13 +22,38 @@ export default function Topbar() {
         }
     };
 
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
+
+    const handleLogout = () => {
+        localStorage.removeItem('admin'); 
+        navigate('/');
+    };
+
     return (
         <div className="topbar d-flex flex-row justify-content-between">
             <h6>{getPageName(location.pathname)}</h6>
             <div className="d-flex flex-row user-profile align-items-center">
                 <img src="/icons/avatar.svg" alt="user-avatar" />
                 <p>Admin</p>
-                <img src="/icons/chevron-down.svg" alt="chevron-down" />
+                <img src="/icons/chevron-down.svg" alt="chevron-down"
+                style={{ cursor: "pointer"}} onClick={toggleDropdown}/>
+
+                {isDropdownOpen && (
+                    <div className="dropdown-menu">
+                        <ul>
+                            <li>
+                                <img src='/icons/user-circle.svg' alt='logout'></img>
+                                <span>Profile</span>
+                            </li>
+                            <li onClick={handleLogout}>
+                                <img src='/icons/logout.svg' alt='logout'></img>
+                                <span>Logout</span>
+                            </li>
+                        </ul>
+                    </div>
+                )}
             </div>
         </div>
     )
