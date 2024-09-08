b1276b35a079228d3ee2f5b2e77b92811fcd23bd
thuanphat611 <phat06112003@gmail.com>
2024-09-08 15:45:21 +0700

    done manage game page

--- admin/src/pages/ManageGame.jsx
+++ admin/src/pages/ManageGame.jsx
@@ -3,5 +3,17 @@ import "../styles/common.css";
 import "../styles/manage.css";
 
 export default function ManageGame() {
-    
+    return (
+      <div className='game-container'>
+        <div className='game'>
+          <img className='game-img' src='/images/quiz.png' alt='Trắc nghiệm' />
+          <h6 className='game-name'>Trắc nghiệm</h6>
+        </div>
+
+        <div className='game'>
+          <img className='game-img' src='/images/lacxu.png' alt='Trắc nghiệm' />
+          <h6 className='game-name'>Trắc nghiệm</h6>
+        </div>
+      </div>
+    )
 }
