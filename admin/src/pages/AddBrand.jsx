ca8069afc1c678090c5c3397d4cded97f0342db2
FelinTran <felintran.work@gmail.com>
2024-09-08 15:03:00 +0700

    add favicon in public/images

---
+++ admin/src/pages/AddBrand.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { useNavigate, Link } from "react-router-dom";
+import "../styles/common.css";
+import "../styles/input.css";
+
+export default function AddBrand() {
+    return(
+        <div>
+
+        </div>
+    );
+}
