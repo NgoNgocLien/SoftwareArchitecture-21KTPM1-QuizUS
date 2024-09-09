3bcac2a672b89daaff78a74512935a7d7b6f21f9
FelinTran <felintran.work@gmail.com>
2024-09-10 06:21:40 +0700

    add brand info

--- brand/src/pages/BrandInfo.jsx
+++ brand/src/pages/BrandInfo.jsx
@@ -1,7 +1,12 @@
+import React from "react";
+import "../styles/common.css";
 
-
-// export default function BrandInfo() {
-//     return (
-    
-//     )
-// }
+export default function BrandInfo() {
+    return (
+        <div className="ctn">
+            <div className="brand-logo-ctn">
+                
+            </div>
+        </div>
+    )
+}
