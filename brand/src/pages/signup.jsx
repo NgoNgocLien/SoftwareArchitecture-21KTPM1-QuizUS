a3378467c4707cb39100344d069b58da28b20b6f
hongphuc14 <nthphuc21@clc.fitus.edu.vn>
2024-09-09 23:41:33 +0700

    done brand signup UI

--- brand/src/pages/signup.jsx
+++ brand/src/pages/signup.jsx
@@ -1,17 +1,15 @@
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from 'react-redux';
+import Loading from '../components/system-feedback/Loading';
+
 import "../styles/common.css";
 import "../styles/signup.css";
-//import { RoleKey } from "../util/config";
 
 export default function Signup(props) {
-    const dispatch = useDispatch();
     const [fullname, setFullname] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    //const [loading, setLoading] = useState(false);
-    //const roleId = localStorage.getItem(RoleKey);
+    const [loading, setLoading] = useState(false);
     const [showNotification, setShowNotification] = useState(false);
 
     const handleSignup = async (e) => {
@@ -65,50 +63,50 @@ export default function Signup(props) {
     };
 
     return (
-        <div className="signup-popup-overlay">
-            <div className="signup-popup">
-                <i className="fa-solid fa-xmark close-button" onClick={props.toggle}></i>
-                <div>
-                    <div className="navbar-brand-signup">
-                        <img src="/images/logo.png" alt="logo" width="22" height="22" className="align-self-center"/>
-                        <br></br>
-                    </div>
-                    
-                    <h2>Create an account</h2>
-                    <p>Welcome! Let's begin the journey with Xplore.</p>
-                </div>
-                
-                <form onSubmit={handleSignup}>
-                    <div className="form-group">
-                        <label>Email</label>
-                        <input type="email" placeholder="Nhập email của bạn" value={email} onChange={e => setEmail(e.target.value)}/>
-                    </div>
-                    <div className="form-group">
-                        <label>Mật khẩu</label>
-                        <input type="password" placeholder="••••••••" value={password} onChange={e => setPassword(e.target.value)}/>
-                        <p className="hint">Must be at least 8 characters.</p>
-                    </div>
+        <div className="signup-brand-container">
+            <div className="signup-form">
+            {loading ? (
+                    <Loading/>
+                ):(
+                    <>
+                        <div>
+                            <div className="navbar-brand-signup">
+                                <img src="/images/logo-dark.png" alt="logo" width="113.82px" height="60px" className="align-self-center"/>
+                                <br></br>
+                            </div>
+                            
+                            <h5>Tạo tài khoản</h5>
+                        </div>
+                        
+                        <form onSubmit={handleSignup}>
+                            <div className="form-group">
+                                <label>Email</label>
+                                <input type="email" placeholder="Nhập email của bạn" value={email} onChange={e => setEmail(e.target.value)}/>
+                            </div>
+                            <div className="form-group">
+                                <label>Mật khẩu</label>
+                                <input type="password" placeholder="••••••••" value={password} onChange={e => setPassword(e.target.value)}/>
+                            </div>
 
-                    {/* <div className="form-row">
-                        <div className="checkbox">
-                            <input type="checkbox" id="remember" />
-                            <label htmlFor="remember">Remember for 30 days</label>
+                            <button type="submit" className="btn-sign-up">Đăng ký</button>
+                        </form>
+
+                        <div className="divider">
+                            <span>Hoặc</span>
                         </div>
 
-                        <div className="forgot-password"><a href="#">Forgot password</a></div>
-                    </div> */}
+                        <button type="button" className="btn-sign-up-google">
+                            <img src="https://res.cloudinary.com/dklt21uks/image/upload/v1725705052/dldlrna0zoe3qqkdehib.png" alt="google" style={{ marginRight: "10px" }}/>
+                            Đăng ký với Google
+                        </button>
 
-                    <button type="submit" className="btn-sign-up">Tạo tài khoản</button>
-                </form>
-                <button type="button" className="btn-sign-up-google">
-                    <img src="/search.png" alt="google" style={{ marginRight: "10px" }}/>
-                    Đăng ký bằng Google
-                </button>
-
-                <div className="sign-in">Đã có tài khoản? 
-                    <span onClick={() => handleNavigate()} style={{color: "var(--scheme-primary)", cursor: "pointer", fontWeight: "600"}}> Đăng nhập</span>
-                </div>
-                {showNotification && <div style={{color: "#004EEA", fontStyle: "italic", fontFamily: "medium-font", marginTop: "10px"}}>Tạo tài khoản thành công!</div>}
+                        <div className="sign-in">Đã có tài khoản? 
+                            <span onClick={() => handleNavigate()} style={{color: "var(--scheme-primary)", cursor: "pointer", fontWeight: "600"}}> Đăng nhập</span>
+                        </div>
+                        
+                        {showNotification && <div style={{color: "#004EEA", fontStyle: "italic", fontFamily: "medium-font", marginTop: "10px"}}>Tạo tài khoản thành công!</div>}
+                    </>
+                )}
             </div>
         </div>
     );
