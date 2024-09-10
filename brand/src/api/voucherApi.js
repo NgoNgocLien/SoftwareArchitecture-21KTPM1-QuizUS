a2b64358c7d4909c5f9020483d5e5f94fde4720d
thuanphat611 <phat06112003@gmail.com>
2024-09-10 21:11:23 +0700

    get data from backend in ManageVoucher

---
+++ brand/src/api/voucherApi.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+const getAll = async (id) => {
+    try {
+      const url = `${process.env.REACT_APP_CAMPAIGN_URL}/api/voucher/search/brand/${id}`;
+      const response = await axios.get(url);
+      if (response?.data) {
+        return response.data
+      }
+      else 
+      return []
+    }
+    catch (err) {
+      console.log(err.message);
+      return []
+    }
+  }
+
+const searchCampaign = async (id , keyword) => {
+  try {
+    let url = `${process.env.REACT_APP_CAMPAIGN_URL}/api/campaign/search/${keyword}`;
+    if (!keyword || keyword.length === 0)
+      url = `${process.env.REACT_APP_CAMPAIGN_URL}/api/campaign/brand/${id}`;
+    const result = await axios.get(url);
+
+    if (result.status === 200) 
+      return result.data;
+    else 
+      return [];
+  } catch (err) {
+    console.log(err.message);
+    return [];
+  }
+}
+
+export{
+  getAll
+}
