aff428ad9f84d69d2913cbcaeddbea9a35702673
FelinTran <felintran.work@gmail.com>
2024-09-10 22:29:32 +0700

    update create event

--- brand/src/App.js
+++ brand/src/App.js
@@ -14,6 +14,7 @@ import Login from './pages/Login';
 import Signup from './pages/signup';
 import BrandInfo from './pages/BrandInfo';
 import CreateEvent from './pages/CreateEvent';
+import CreateGame from './pages/CreateGame';
 
 function Layout() {
   return (
@@ -42,6 +43,7 @@ function App() {
           <Route path="/info" element={<BrandInfo />} />
           <Route path="/edit" element={<UpdatePlayer />} />
           <Route path="/create-event" element={<CreateEvent />} />
+          <Route path="/create-game" element={<CreateGame />} />
         </Route>
       </Routes>
     </Router>
--- brand/src/pages/CreateGame.jsx
+++ brand/src/pages/CreateGame.jsx
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/common.css";
 import "../styles/input.css";
 
 export default function CreateGame() {
+    const [questions, setQuestions] = useState(Array(10).fill({ question: '', options: ['', '', '', ''], correctAnswer: 0 }));
+    const [reward, setReward] = useState(0);
+
+    const handleQuestionChange = (index, field, value) => {
+        const updatedQuestions = [...questions];
+        if (field === 'question') {
+            updatedQuestions[index].question = value;
+        } else {
+            updatedQuestions[index].options[field] = value;
+        }
+        setQuestions(updatedQuestions);
+    };
+
+    const handleCorrectAnswerChange = (index, value) => {
+        const updatedQuestions = [...questions];
+        updatedQuestions[index].correctAnswer = value;
+        setQuestions(updatedQuestions);
+    };
+
+    const handleRewardChange = (e) => {
+        setReward(e.target.value);
+    };
+
+    const handleSubmit = () => {
+        console.log("Quiz Data:", { questions, reward });
+        alert("Quiz Created Successfully!");
+    };
+
     return (
-        <div>
-            
+        <div className="ctn">
+            <div className="input-ctn">
+                <h6>Tạo trò chơi</h6>
+            </div>
         </div>
     )
 }
