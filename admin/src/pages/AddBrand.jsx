05dc79f4f0c467e0e6f331bfb883994403af3153
hongphuc14 <nthphuc21@clc.fitus.edu.vn>
2024-09-11 20:56:45 +0700

    add brand logic

--- admin/src/pages/AddBrand.jsx
+++ admin/src/pages/AddBrand.jsx
@@ -5,7 +5,7 @@ import "../styles/input.css";
 import { confirmAlert } from 'react-confirm-alert';
 import 'react-confirm-alert/src/react-confirm-alert.css'; 
 
-import { createBrand } from '../api/brandApi';
+import { createBrand, uploadImgToCloudinary } from '../api/brandApi';
 
 export default function AddBrand() {
     const navigate =  useNavigate();
@@ -16,20 +16,44 @@ export default function AddBrand() {
     const [email, setEmail] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    
+    const [phone, setPhone] = useState('');
+
+    const [logo, setLogo] = useState('');
+    const [logoFile, setLogoFile] = useState(null);
+
     const onCancel  = () => {
         navigate(`/brand`);
     }
 
     const onSave = async () => {
+        var imageUrl;
+        if (logoFile) {
+            try {
+                imageUrl = await uploadImgToCloudinary(logoFile);
+
+                if (imageUrl) {
+                    console.log('imageUrl:', imageUrl);
+                } else {
+                    console.error('Failed to upload image');
+                }
+            } catch (error) {
+                console.error('Error uploading image:', error);
+            }
+        }
+
         let newData = {
             name,
             field,
             address,
-            email,
+            lat: "",
+            long: "",
             username,
-            pwd: password
+            pwd: password,
+            phone,
+            email,
+            logo: imageUrl
         }
+
         let success = await createBrand(newData);
         if (success) {
             confirmAlert({
@@ -56,16 +80,31 @@ export default function AddBrand() {
         }
     }
 
+    const handleLogoChange = (e) => {
+        const file = e.target.files[0];
+        console.log("file: ", file)
+
+        setLogoFile(file);
+
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            setLogo(reader.result); 
+        };
+        if (file) {
+            reader.readAsDataURL(file);
+        }
+    };
+
     return(
         <div className='ctn'>
             <div className='brand-logo-ctn'>
-                <img src="/images/placeholder-img.jpg" alt="brand-logo" />
+                <img src={logo?.length > 0 ? logo : "/images/placeholder-img.jpg"} alt="brand-logo" className='profile-avatar'/>
                 <div className="upload-btn-ctn">
-                    <button className="upload-btn"> {/* Button giả */}
+                    <button className="upload-btn"> 
                         <img src="/icons/camera-plus.svg" alt="upload-img" />
                         Chọn ảnh
                     </button> 
-                    <input type="file" id="file-upload" /> {/* Code backend cho input này nha */}
+                    <input type="file" id="file-upload" accept="image/*" onChange={handleLogoChange}/> 
                 </div>
             </div>
             
@@ -104,10 +143,10 @@ export default function AddBrand() {
                     <div className="form-group">
                         <label htmlFor="field">Lĩnh vực</label>
                         <select id="field" value={field} onChange={(e) => { setField(e.target.value) }}>
-                            <option value="restaurant">Nhà hàng</option>
-                            <option value="cafe">Cafe & Bánh</option>
-                            <option value="shopping">Mua sắm</option>
-                            <option value="entertainment">Giải trí</option>
+                            <option value="Nhà hàng">Nhà hàng</option>
+                            <option value="Cafe & Bánh">Cafe & Bánh</option>
+                            <option value="Mua sắm">Mua sắm</option>
+                            <option value="Giải trí">Giải trí</option>
                         </select>
                     </div>
                 </div>
@@ -120,11 +159,11 @@ export default function AddBrand() {
                     </div>
                 </div>
 
-                {/* Website */}
+                {/* Phone */}
                 <div className="form-row">
                     <div className="form-group">
-                        <label htmlFor="website">Trang web</label>
-                        <input type="url" id="website" placeholder="Nhập URL trang web" />
+                        <label htmlFor="phone">Số điện thoại</label>
+                        <input type="text" id="phone" placeholder="Nhập số điện thoại" value={phone} onChange={(e) => {setPhone(e.target.value)}}/>
                     </div>
                 </div>
 
