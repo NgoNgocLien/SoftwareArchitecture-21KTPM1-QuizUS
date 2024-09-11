d6b6d231d5fd54ee83d9f15559bff780f7b8c436
hongphuc14 <nthphuc21@clc.fitus.edu.vn>
2024-09-11 11:15:24 +0700

    add be dashboard player by game stats

--- admin/src/api/statsApi.js
+++ admin/src/api/statsApi.js
@@ -31,7 +31,20 @@ const getVoucherStats = async () => {
   }
 }
 
+const getPlayerByGameStats = async () => {
+  try {
+    const url = `${process.env.REACT_APP_CAMPAIGN_URL}/api/game/playerByGame`;
+    const response = await axios.get(url);
+    console.log(response)
+    return response.data;
+  }
+  catch (err) {
+    console.log(err.message);
+    return []
+  }
+}
+
 
 export {
-  getStats, getVoucherStats
+  getStats, getVoucherStats, getPlayerByGameStats
 };
