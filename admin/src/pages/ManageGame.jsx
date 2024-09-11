82fab56cd264df955e24b1d701c3c59a91661d3b
FelinTran <felintran.work@gmail.com>
2024-09-11 20:42:28 +0700

    done manage game and game settings link

--- admin/src/pages/ManageGame.jsx
+++ admin/src/pages/ManageGame.jsx
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import "../styles/common.css";
 import "../styles/manage.css";
 
 export default function ManageGame() {
+    const navigate = useNavigate();
     return (
       <div className='game-container'>
-        <button className='game'>
+        <button className='game' onClick={() => {navigate('/game-settings')}} >
           <img className='game-img' src='/images/quiz.png' alt='Trắc nghiệm' />
           <h6 className='game-name'>Trắc nghiệm</h6>
         </button>
 
-        <button className='game'>
+        <button className='game' onClick={() => {navigate('/game-settings')}} >
           <img className='game-img' src='/images/lacxu.png' alt='Trắc nghiệm' />
           <h6 className='game-name'>Lắc vật phẩm</h6>
         </button>
