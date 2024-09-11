3f778382ff56e44e3efe80853e68c0740c79a4f2
hongphuc14 <nthphuc21@clc.fitus.edu.vn>
2024-09-12 05:25:44 +0700

    update game setting

--- admin/src/pages/GameSettings.jsx
+++ admin/src/pages/GameSettings.jsx
@@ -2,13 +2,18 @@ import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import "../styles/common.css";
 import "../styles/input.css";
-import { getGameById } from '../api/gameApi';
+import { getGameById, updateGame } from '../api/gameApi';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css'; 
 
 export default function GameSettings() {
     const { id_game } = useParams();
     const navigate = useNavigate();
 
     const [field, setField] = useState('');
+    const [award, setAward] = useState('');
+    const [question, setQuestion] = useState('');
+    const [voucher, setVoucher] = useState('');
 
     useEffect(() => {
         const getData = async () => {
@@ -17,10 +22,48 @@ export default function GameSettings() {
             console.log(data)
             if (data) {
                 setField(data.name || '');
+                if(data.name == "Trắc nghiệm")
+                    setAward("Xu thưởng")
+                else
+                    setAward("Mảnh ghép")
+                setQuestion("10");
+                setVoucher("1");
             }
         }
         getData();
     }, [id_game]);
+
+    const onSave = async () => {
+        let updatedData = {
+            id_game,
+            name: field
+        }
+        
+        let success = await updateGame(updatedData);
+        if (success) {
+            confirmAlert({
+                message: 'Chỉnh sửa trò chơi thành công!',
+                buttons: [
+                    {
+                        label: 'Xác nhận',
+                        onClick: () => {
+                            navigate(`/game`);
+                        }
+                    }
+                ]
+            });
+        }
+        else {
+            confirmAlert({
+                message: 'Chỉnh sửa trò chơi thất bại!',
+                buttons: [
+                    {
+                        label: 'Xác nhận'
+                    }
+                ]
+            });
+        }
+    }
 
     return (
         <div className="ctn">
@@ -45,7 +88,7 @@ export default function GameSettings() {
                 <div className="form-row">
                     <div className="form-group" style={{width: '100%'}}>
                         <label style={{ fontFamily: 'semibold-font', fontSize: '14px', marginBottom: '4px'}}>Số lượng câu hỏi</label>
-                        <input type="number" style={{width: '100%', boxSizing: 'border-box', fontSize: '16px'}} value="10"></input>
+                        <input type="number" style={{width: '100%', boxSizing: 'border-box', fontSize: '16px'}} value={question} onChange={(e) => {setQuestion(e.target.value)}}></input>
                     </div>
                 </div>
 
@@ -64,9 +107,9 @@ export default function GameSettings() {
                 </div> */}
                 <div className="form-row" style={{ width: '30%'}}>
                     <label style={{ fontFamily: 'semibold-font', fontSize: '14px', marginBottom: '4px'}}>Phần thưởng</label>
-                    <input type="checkbox" style={{ height: '18px', width: '18px', margin: '0'}} />
+                    <input type="checkbox" style={{ height: '18px', width: '18px', margin: '0'}} checked={award === "Xu thưởng"} onChange={(e) => {setAward(e.target.value)}}/>
                     <label style={{ margin: '0'}}>Xu thưởng</label>
-                    <input type="checkbox" style={{ height: '18px', width: '18px', margin: '0'}} />
+                    <input type="checkbox" style={{ height: '18px', width: '18px', margin: '0'}} checked={award === "Mảnh ghép"} onChange={(e) => {setAward(e.target.value)}}/>
                     <label style={{ margin: '0'}}>Mảnh ghép</label>
                 </div>
                 
@@ -75,14 +118,14 @@ export default function GameSettings() {
                 <div className="form-row">
                     <div className="form-group">
                         <label style={{ fontFamily: 'semibold-font', fontSize: '14px', marginBottom: '4px'}}>Số loại voucher</label>
-                        <input type="number" style={{width: '100%', boxSizing: 'border-box', fontSize: '16px'}} value="1"></input>
+                        <input type="number" style={{width: '100%', boxSizing: 'border-box', fontSize: '16px'}} value={voucher} onChange={(e) => {setVoucher(e.target.value)}}></input>
                     </div>
                 </div>
 
                 {/* Buttons */}
                 <div className="button-group">
                     <button className="cancel-btn" onClick={() => {navigate('/game')}}>Hủy</button>
-                    <button className="save-btn">Lưu</button>
+                    <button className="save-btn" onClick={() => {onSave()}}>Lưu</button>
                 </div>
 
             </div>
